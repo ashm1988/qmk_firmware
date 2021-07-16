cfc6d1b22f2227654184d71967b07c532a0bf2f5
ashm1988 <ashisgreat15@hotmail.com>
2021-07-17 01:53:52 +1000

    add left alt to insert

--- keyboards/gmmk/pro/keymaps/ashm1988/keymap.c
+++ keyboards/gmmk/pro/keymaps/ashm1988/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   RALT_T(KC_F10),  RCTL_T(KC_F11),  RSFT_T(KC_F12),  KC_INS, TD(TD_MUTE_PLAY),
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   RALT_T(KC_F10),  RCTL_T(KC_F11),  RSFT_T(KC_F12),  LALT_T(KC_INS), TD(TD_MUTE_PLAY),
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_BSPC,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
